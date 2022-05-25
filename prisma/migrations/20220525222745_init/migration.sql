cad8f45c62db25646c9a6faee211b68320bb92bf
ronaldjunior2 <ronaldjunior2@hotmail.com>
2022-05-25 19:31:55 -0300

    feat: config prisma

---
+++ prisma/migrations/20220525222745_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Resources" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Resources_pkey" PRIMARY KEY ("id")
+);
